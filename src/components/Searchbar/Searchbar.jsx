f5636f56bfe4ad3b00475b72e841dbbb2abf3347
Irina Romjalis <iromjalis@gmail.com>
2021-09-22 00:29:09 +0300

    Update Searchbar.jsx
    on hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,44 +1,38 @@
-import React, { PureComponent } from "react";
-// import PropTypes from 'prop-types';
-// import { Test } from './Searchbar.styles';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-class Searchbar extends PureComponent {
-  state = { query: "" };
-
-  handleChange = (e) => {
-    this.setState({ query: e.currentTarget.value });
+const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState("");
+  const handleChange = (e) => {
+    setQuery(e.currentTarget.value);
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    this.props.onSubmit(this.state.query);
+    onSubmit(query);
 
-    this.setState({ query: "" });
+    setQuery("");
   };
-  render() {
-    const { query } = this.state;
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
 
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
-
-          <input
-            onChange={this.handleChange}
-            className="SearchForm-input"
-            value={query}
-            type="text"
-            autoComplete="off"
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          onChange={handleChange}
+          className="SearchForm-input"
+          value={query}
+          type="text"
+          autoComplete="off"
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   // bla: PropTypes.string,
